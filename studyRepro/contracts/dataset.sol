1315777c4a2b906bbb2f2b8097cc28d71ea7f0c7
Eric Frankel <eric-frankel@cox.net>
2018-08-04 13:57:15 -0700

    migrated to contracts folder

---
+++ studyRepro/contracts/dataset.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.24;
+
+import "./ownable.sol";
+
+contract DataSet is Ownable {
+
+  event NewData(uint dataId, string testparam, string testresult);
+
+  struct Data {
+    string TestParameters;  //inputs
+    string TestResults;     //outputs
+    string Field;           //field of study
+    uint16 verifCount;
+    uint16 unverifCount;
+  }
+
+  Data[] public datas;
+
+  mapping (uint => address) public dataToOwner;
+  mapping (address => uint) public ownerDataCount;
+
+  function _createData(string _testparam, string _testresult, string _field) internal returns (uint) {
+    uint id = datas.push(Data(_testparam, _testresult, _field, 0, 0)) - 1;
+    dataToOwner[id] = msg.sender;
+    ownerDataCount[msg.sender]++;
+    NewData(id, _testparam, _testresult);
+    return id;
+  }
+
+}

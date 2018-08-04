6f846ed581326bd49aba837ba2d10ffe95476567
yidansun1203 <yidansun.teddy@gmail.com>
2018-08-04 14:53:14 -0700

    test

--- studyRepro/src/js/app.js
+++ studyRepro/src/js/app.js
@@ -9,12 +9,12 @@ App = {
       var petTemplate = $('#petTemplate');
 
       for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+        petTemplate.find('.panel-title').text(data[i].testParameter);
+       // petTemplate.find('img').attr('src', data[i].picture);
+        petTemplate.find('.pet-breed').text(data[i].testParameter);
+        petTemplate.find('.pet-age').text(data[i].fields);
+       // petTemplate.find('.pet-location').text(data[i].location);
+       // petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
 
         petsRow.append(petTemplate.html());
       }
